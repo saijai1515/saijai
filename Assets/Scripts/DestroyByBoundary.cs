ee6167204db50fcae5f65d898eb88f98506365be
saijai15 <saijai.petl@bumail.net>
2017-02-21 22:20:27 +0700

    10.Explosions

--- Assets/Scripts/DestroyByBoundary.cs
+++ Assets/Scripts/DestroyByBoundary.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DestroyByBoundary : MonoBehaviour {
-
-	// Use this for initialization
+	
 	void OntriggreExit(Collider other)
 	{
 		Destroy (other.gameObject);	
--- Assets/Scripts/DestroyContace.cs
+++ Assets/Scripts/DestroyContace.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DestroyContace : MonoBehaviour {
+	public GameObject explosion;
+	public GameObject playerExplosion;
 
-	// Use this for initialization
 	void OnTriggerEnter(Collider other)
 	{
 		if (other.tag == "Boundary")
 	{
 		return;
 	}
+		Instantiate (playerExplosion, transform.position, transform.rotation);
+		if (other.tag == "Player") {
+			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+
+		}
+
 		Destroy (other.gameObject);
 		Destroy (gameObject);
 		
